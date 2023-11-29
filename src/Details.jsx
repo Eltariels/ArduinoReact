2ab984c4430417c7b64c24c29a9e573bff12db52
Eltariels <theo.borella.hc@gmail.com>
2023-11-29 16:29:48 +0100

    Create Details.jsx
    create details jsx

---
+++ src/Details.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './css/index.css';
+
+const detailsconst = {
+  name: 'Configuration',
+};
+
+function Details() {
+  return (
+    <>
+      <div className='details'>
+        <h2>{detailsconst.name}</h2>
+        
+      </div>
+    </>
+  )
+}
+
+export default Details 
