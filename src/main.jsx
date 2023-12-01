40b5f93b75de2cde6e651e3dab7fb24352ab5ab9
Eltariels <theo.borella.hc@gmail.com>
2023-12-01 09:31:28 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './css/index.css';
 
+console.log('%c LA FORET ENCHANTEE', 'background: #4CAF50; color: white; padding: 10px; border-radius: 15px; border: 2px solid #006400; font-size: 18px; font-weight: bold;');
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
